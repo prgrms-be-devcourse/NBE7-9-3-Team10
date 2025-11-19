01b870188a91dd5bbac025d1299925eb7d046fdb
HongRae-Kim <ghdfo7882@gmail.com>
2025-11-19 15:20:57 +0900

    refactor: UnimateApplicationTests 마이그레이션

---
+++ backend/unimate/src/test/java/com/unimate/UnimateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unimate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+internal class UnimateApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
