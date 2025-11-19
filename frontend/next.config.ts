6ab9faa6d3dbdb840fd683fb81ad0ece52f3ea4a
ahnbs <ahnbs0@gmail.com>
2025-11-19 14:28:33 +0900

    feat : cd진행중 도커파일 작성

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from 'next'
 
 
 const nextConfig: NextConfig = {
-async rewrites() {
-return [
-{
-source: '/api/:path*',
-destination: `${process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8080'}/api/:path*`,
-},
-]
-},
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:8080'}/api/:path*`,
+      },
+    ]
+  },
 }
 
 
